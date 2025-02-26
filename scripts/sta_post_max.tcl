181cae75005f3fef0f8f2030b3279a3ae7775675
Ri <rithanipriyanka1602@gmail.com>
2025-02-25 22:03:54 -0500

    Add files via upload

---
+++ scripts/sta_post_max.tcl
@@ -0,0 +1,22 @@
+
+set link_path  [list ../ref/db_nldm/saed14rvt_tt0p8v25c.db ] 
+
+read_verilog [list ../source/johnson_icc.v]
+
+current_design johnson
+link
+
+read_sdc ../source/johnson_icc.sdc
+read_parasitics ../source/johnson.spef.max
+
+set_case_analysis 0 [get_port r]
+set_case_analysis 0 [get_port SE]
+
+report_constraint -all_violators -significant_digits 4 > ../results/johnson.max_constr.rpt
+report_timing -delay_type max -nworst 40 -significant_digits 4 > ../results/johnson.max_timing.rpt
+
+write_sdf ../results/johnson.max.sdf
+
+
+
+
